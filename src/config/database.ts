bd47b48ea303e04e61cc92a74145acb88fcf7cb0
Kazi Wakil Ahmed <ahmedwakil66@gmail.com>
2024-09-05 03:48:28 +0600

    listens for connection interruption

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,4 @@
-import mongoose, { Error } from "mongoose";
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
@@ -6,6 +6,9 @@ const connectDB = async () => {
       dbName: process.env.DB_NAME,
     });
     console.log("MongoDB Connected");
+    mongoose.connection.on('error', err => {
+      console.error("mongo connection interrupted: ", err);
+    });
   } catch (error) {
     console.error(error);
     process.exit(1);
