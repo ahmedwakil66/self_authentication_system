117d6f64b137127b7aef47a18500e190e882f0ca
Kazi Wakil Ahmed <ahmedwakil66@gmail.com>
2024-09-09 00:24:37 +0600

    defined blog-post model

--- src/models/blogModel.ts
+++ src/models/blogModel.ts
@@ -7,6 +7,12 @@ export enum BlogPostTag {
   Math = "math",
 }
 
+export enum BlogPostStatus {
+  Draft = "draft",
+  Private = "private",
+  Published = "published",
+}
+
 export interface IBlogPost {
   title: string;
   body: string;
@@ -17,6 +23,9 @@ export interface IBlogPost {
     funny: number;
   };
   views: number;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
   author: mongoose.Types.ObjectId;
 }
 
@@ -24,19 +33,28 @@ const blogPostSchema = new mongoose.Schema<IBlogPost>(
   {
     title: { type: String, required: true, trim: true },
     body: { type: String, required: true },
-    tags: { type: [String], enum: Object.keys(BlogPostTag), default: [] },
+    tags: { type: [String], enum: Object.values(BlogPostTag), default: [] },
     views: { type: Number, default: 0 },
     reactions: {
       likes: { type: Number, default: 0 },
       dislikes: { type: Number, default: 0 },
       funny: { type: Number, default: 0 },
     },
+    status: {
+      type: String,
+      enum: Object.values(BlogPostStatus),
+      default: "published",
+      required: true,
+    },
     author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
+    createdAt: Date,
+    updatedAt: Date
   },
   {
+    timestamps: true,
     methods: {},
   }
 );
